de6d66da9780efd670cd2ffe8306a183fa9a8c8e
parsamanesh <stooormix@gmail.com>
2023-07-21 00:47:47 +0330

    responsive home page

--- pages/index.tsx
+++ pages/index.tsx
@@ -29,6 +29,7 @@ const Home = () => {
         particlesLoaded={particlesLoaded}
         init={particlesInit}
         loaded={particlesLoaded}
+        //@ts-ignore
         options={ParticlesConfig}
         height="100vh"
         width="100vw"
@@ -38,24 +39,23 @@ const Home = () => {
       </div>
 
 
-      <div className="relative px-[5%] w-full h-full flex flex-row-reverse z-20 justify-evenly items-center">
-        <div className="flex h-full w-1/2 text-dropdown z-10 flex-col justify-center items-end">
-          <h1 className=" dir_rtl text-6xl">
+      <div className="relative  w-full h-full flex flex-col-reverse md:flex-row-reverse z-20 justify-evenly items-center lg:px-[5%]">
+        <div className="flex md:h-full w-full md:w-1/2 text-dropdown z-10 flex-col justify-center items-center md:items-end">
+          <h1 className=" dir_rtl text-2xl md:text-4xl lg:text-6xl">
             فقط چند خط <span className="animate-ping delay_100">.</span>
             <span className="animate-ping delay_200">.</span>
             <span className="animate-ping delay_300">.</span>
           </h1>
           <br />
-          <i>فقط با نوشتن چند خط در روز میتونی معجزه رو احساس کنی</i>
+          <i className=" text-xs md:text-sm lg:text-base">فقط با نوشتن چند خط در روز میتونی معجزه رو احساس کنی</i>
           <br />
-
           <div className="">
           <Link href="/journal-info" target="_blank" className="mr-4">
               <motion.button
                 initial={{ opacity: 0 }}
                 animate={{ opacity: 1 }}
                 transition={{ delay: 0.6 }}
-                className="px-4 py-2 rounded-lg text-white bg-button text-[16px] border border-dotin hover:bg-buttonHover duration-300">
+                className="px-4 py-2 rounded-lg text-white bg-button text-xs md:text-sm lg:text-[16px] border border-dotin hover:bg-buttonHover duration-300">
                 چرا ژورنال؟
               </motion.button>
             </Link>
@@ -64,14 +64,14 @@ const Home = () => {
                 initial={{ opacity: 0 }}
                 animate={{ opacity: 1 }}
                 transition={{ delay: 0.6 }}
-                className="px-4 py-2 rounded-lg text-white bg-button text-[16px] border border-dotin hover:bg-buttonHover duration-300">
+                className="px-4 py-2 rounded-lg text-white bg-button text-xs md:text-sm lg:text-[16px] border border-dotin hover:bg-buttonHover duration-300">
                 نوشتن
               </motion.button>
             </Link>
 
           </div>
         </div>
-        <div className="w-1/2  z-10 flex justify-center items-center">
+        <div className="w-1/3  z-10 flex justify-center items-center lg:w-1/2">
           <Image
             src={dotin_nav_logo}
             alt="dotin logo"
