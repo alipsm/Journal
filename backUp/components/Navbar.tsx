fd5bba8bd1253f02fb750b426f949deebc44fe68
parsamanesh <stooormix@gmail.com>
2023-08-22 13:34:46 +0330

    replace includes with concat for hidden nav bar in browser route dashboard/*

--- backUp/components/Navbar.tsx
+++ backUp/components/Navbar.tsx
@@ -44,7 +44,7 @@ const Navbar = () => {
 
   // hidden nav bar in dashboard route
   const hiddenRoutes = ["/dashboard"];
-  if (hiddenRoutes.includes(router.pathname)) return null;
+  if (hiddenRoutes.concat(router.pathname)) return null;
 
   return (
     <div className="w-full shadow-navbarShadow h-20  sticky top-0 z-50 bg-white px-4">
